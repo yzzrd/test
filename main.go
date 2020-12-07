5cac899635822b419d059226dd39953bf397574b
zrd <yzzrd@126.com>
2020-12-07 10:25:36 +0800

    add test

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"./model"
+)
+
+func main(){
+	fmt.Println("hellofo world")
+	fmt.Println("first commit")
+	model.Testfunc()
+}
---
+++ model/test.go
@@ -0,0 +1,7 @@
+package model
+
+import "fmt"
+
+func Testfunc(){
+	fmt.Println("This is testfunc")
+}
